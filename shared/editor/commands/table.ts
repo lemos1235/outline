a9f3c164cac23083a34a40dcdff02b38ef7e15e6
lemos <xfe1235@gmail.com>
2025-12-23 13:27:44 +0800

    fix(editor): add BOM to CSV export

--- shared/editor/commands/table.ts
+++ shared/editor/commands/table.ts
@@ -161,7 +161,7 @@ export function exportTable({
         )
         .join("\n");
 
-      const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+      const blob = new Blob(["\uFEFF" + csv], { type: "text/csv;charset=utf-8;" });
       const url = URL.createObjectURL(blob);
       const a = document.createElement("a");
       a.href = url;
